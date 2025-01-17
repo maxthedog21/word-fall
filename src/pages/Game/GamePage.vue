b516f68d901a60f2a3c16b315200b8ce70d324cf
maximilianmoore <maximilianmoore44@gmail.com>
2025-01-17 18:14:04 -0500

    init commit
    endless core game finished

---
+++ src/pages/Game/GamePage.vue
@@ -0,0 +1,40 @@
+<template>
+    <canvas id="canvas"></canvas>
+</template>
+<script>
+export default{
+    name: "GamePage",
+    components: {
+
+    },
+         data(){
+             return {
+                 clientId: "",
+                 redirectURI: "",
+                 canvas: null,
+                 ctx: null,
+             };
+         },
+     mounted(){
+       //  this.start();
+         window.addEventListener("resize", this.updateCanvasSize);
+         this.canvas = document.getElementById('canvas');
+         this.ctx = this.canvas.getContext("2d");
+         this.canvas.width = document.body.offsetWidth;
+         this.canvas.height = document.body.offsetHeight;
+         this.ctx.font = "30px Arial";
+         this.ctx.fillStyle= "white";
+         this.ctx.fillText("test", 10, 180);
+         //this.generateStars(128);
+
+         this.last = this.timestamp();
+        // window.requestAnimationFrame(this.runStars);
+     },
+     methods: {
+      updateCanvasSize(){
+         this.canvas.width = document.body.offsetWidth;
+         this.canvas.height = document.body.offsetHeight;
+      },
+     },
+}
+</script>
